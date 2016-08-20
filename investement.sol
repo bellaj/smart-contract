7b6252de8390792d366c7a05317bf15839da6a17
badr <bellaj.badr@gmail.com>
2016-08-20 13:26:43 +0100

    pay back and register stake holder

--- investement.sol
+++ investement.sol
@@ -8,21 +8,25 @@ address curator; // contract creator
 uint public contract_balance;
 bool order;
 bool issent;
+uint numStakeHolders;
+Investor[] list_investor;
+Entrepreneur entrep;
 
 struct Entrepreneur {
-  uint key;
-  uint loan;
+
   address ent_add;
+  uint ratio;
 }
 
 struct Investor {
   uint investement;
   address inv_add;
+  uint ratio;
 }
 
 
 
-mapping (address => Investor) public hubs;
+mapping (address => Investor[] )   investemments;
 
 function investement (){ // constructor set the contract owner who accept the order to winthdraw
   curator=msg.sender;
@@ -30,6 +34,20 @@ function investement (){ // constructor set the contract owner who accept the or
   contract_balance= this.balance;
 }
 
+function register_stakholders(address[] investor_add ,uint[] investement, uint[] inve_ratios, address entrepreneur_address, uint ratio_entr) onlyOwner{
+
+
+for (var i=0; i<=  investor_add.length;i++)
+{
+  //var queue = requestQueue; // stores reference to storage
+  list_investor[i]=Investor(investement[i],investor_add[i],inve_ratios[i]);
+
+}
+entrep=Entrepreneur(entrepreneur_address,ratio_entr);
+investemments[entrepreneur_address] = list_investor;
+numStakeHolders=investor_add.length;
+//investemments[entrepreneur]=investor;
+}
 
 modifier onlyOwner {
     if (msg.sender != curator)
@@ -58,8 +76,21 @@ if (order) { order = false;
 
 }
 
-function pay_back (){
+function pay_back (){ //
 //msg.balance;
+uint bal= this.balance;
+address ent=msg.sender;
+
+ list_investor = investemments[ent] ;
+address inv_add;
+uint rat;
+for (var i=0; i<=  list_investor.length;i++)
+{
+  inv_add=  list_investor[i].inv_add ; rat=list_investor[i].ratio;
+  //var queue = requestQueue; // stores reference to storage
+ send_( ( bal * rat/100),inv_add );
+
+}
 
 
 }
